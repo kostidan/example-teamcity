45c56182f17d850bbf22a9d65083a388ce7dea24
vagrant <vagrant@vagrant.vm>
2022-04-17 18:51:00 +0000

    sayHunter

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -10,4 +10,8 @@ public class Welcomer{
 	public String sayStatus() {
 		return "Hello! My status is Good";
 	}
+	public String sayHunter() {
+        return "I'm a great dragon hunter.";
+    }
 }
+
